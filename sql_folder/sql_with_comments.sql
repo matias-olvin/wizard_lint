a9efb485b8dd8dd4e8426eb72ef37ab92c1c845a
matias-olvin <matias@olvin.com>
2024-03-01 18:54:45 +0000

    added git ignore

--- sql_folder/sql_with_comments.sql
+++ sql_folder/sql_with_comments.sql
@@ -5,11 +5,26 @@ CREATE OR REPLACE TABLE
     AS
 SELECT
     a.*,
+    IFNULL(active_places_count, 0) AS active_places_count,
+    IF(IFNULL(active_places_count, 0) > 0, TRUE, FALSE) AS monthly_visits_availability,
+    IF(
+        IFNULL(high_granularity_count, 0) > 0,
+        TRUE,
+        FALSE
+    ) AS patterns_availability
 FROM
     `storage-prod-olvin-com.sg_places.malls_base` a
     LEFT JOIN (
+        SELECT
+            fk_parents,
+            COUNTIF(activity IN ('active', 'limited_data')) AS active_places_count,
+            COUNTIF(activity = 'active') AS high_granularity_count
+        FROM
             (
                 SELECT
+                    pid,
+                    fk_parents,
+                    activity
                 FROM
                     `storage-prod-olvin-com.postgres_final.SGPlaceRaw`
                     -- `storage-prod-olvin-com.postgres.SGPlaceRaw`
@@ -21,4 +36,4 @@ FROM
             )
         GROUP BY
             fk_parents
-    )
+    ) ON a.pid = fk_parents
