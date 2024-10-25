90faae1c5391d58e8fecd6d6c02e77b647c6e66a
matias-olvin <matias@olvin.com>
2024-10-25 14:04:44 +0100

    Refactor obtain_sql_string_from_file_path to read SQL string from file

--- tests/test_retrieve.py
+++ tests/test_retrieve.py
@@ -1,7 +1,9 @@
 from wizard_lint.retrieve import obtain_sql_string_from_file_path, obtain_file_paths_from_directory
 
 def test_obtain_sql_string_from_file_path():
-    assert obtain_sql_string_from_file_path() == "SELECT * FROM project.dataset.table"
+
+    path = "./sql_folder/test_sql_open.sql"
+    assert obtain_sql_string_from_file_path(path) == "SELECT * FROM `project.dataset.table`"
 
 def test_obtain_file_paths_from_directory():
     assert obtain_file_paths_from_directory() == ["./sql_folder/dummy.sql"]
--- wizard_lint/retrieve.py
+++ wizard_lint/retrieve.py
@@ -1,8 +1,13 @@
 from typing import List
 
 
-def obtain_sql_string_from_file_path() -> str:
-    return "SELECT * FROM project.dataset.table"
+
+def obtain_sql_string_from_file_path(path: str) -> str:
+
+    with open(path, 'r') as file:
+        sql_string = file.read()
+
+        return sql_string
 
 def obtain_file_paths_from_directory() -> List[str]:
     return ["./sql_folder/dummy.sql"]
