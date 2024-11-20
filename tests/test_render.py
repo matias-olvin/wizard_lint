98c0285c45799df3c22d74f6de2964e1b138100e
matias-olvin <matias@olvin.com>
2024-11-20 15:56:43 +0000

    write tests for render_table_string

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,17 +1,7 @@
 from wizard_lint.render import render_table_string, obtain_table_strings
 
-table_name_str = "project.dataset.table"
 
-config = {
-    "project_param": "project",
-    "dataset_param": "dataset",
-    "table_param": "table",
-}
-
-
-def test_render_table_string():
-    assert render_table_string() == "{{ params['project_param'] }}.{{ params['dataset_param'] }}.{{ params['table_param'] }}"
-
+# test obtain_table_strings
 def test_obtain_table_strings_single():
     sql_string = "SELECT * FROM project.dataset.table"
     assert obtain_table_strings(sql_string) == ["project.dataset.table"]
@@ -31,3 +21,60 @@ def test_obtain_table_strings_mixed():
 def test_obtain_table_strings_with_airflow_vars_and_no_tables():
     sql_string = "SELECT * FROM table WHERE column = '{{ var.value.variable_name }}'"
     assert obtain_table_strings(sql_string) == []
+
+# test render_table_string
+
+table_string = "project.dataset.table"
+
+config = {
+    "dataset_param": "dataset",
+    "table_param": "table",
+}
+
+def test_render_table_string_basic():
+    assert render_table_string(config=config, table_string=table_string) == "project.{{ params['dataset_param'] }}.{{ params['table_param'] }}"
+    
+def test_render_table_string_with_different_config():
+    config = {
+        "ds_param": "dataset",
+        "tbl_param": "table",
+    }
+    assert render_table_string(config=config, table_string=table_string) == "project.{{ params['ds_param'] }}.{{ params['tbl_param'] }}"
+
+def test_render_table_string_with_missing_config():
+    config = {
+        "dataset_param": "dataset",
+    }
+    try:
+        render_table_string(config=config, table_string=table_string)
+        assert False, "Expected KeyError"
+    except KeyError:
+        pass
+
+def test_render_table_string_with_extra_config():
+    config = {
+        "dataset_param": "dataset",
+        "table_param": "table",
+        "extra_param": "extra",
+    }
+    assert render_table_string(config=config, table_string=table_string) == "project.{{ params['dataset_param'] }}.{{ params['table_param'] }}"
+
+def test_render_table_string_with_empty_config():
+    config = {}
+    try:
+        render_table_string(config=config, table_string=table_string)
+        assert False, "Expected KeyError"
+    except KeyError:
+        pass
+
+def test_render_table_string_with_invalid_table_string():
+    config = {
+        "dataset_param": "dataset",
+        "table_param": "table",
+    }
+    invalid_table_string = "project.dataset"
+    try:
+        render_table_string(config=config, table_string=invalid_table_string)
+        assert False, "Expected ValueError"
+    except ValueError:
+        pass
