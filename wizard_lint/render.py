bcc797a7c456c40997ebdd20eebb81a87510aad3
matias-olvin <matias@olvin.com>
2024-11-20 14:19:59 +0000

    update regex pattern to ignore vars in airflow

--- wizard_lint/render.py
+++ wizard_lint/render.py
@@ -1,7 +1,12 @@
 from typing import List
+import re
 
-def obtain_table_strings() -> List[str]:
-    return ["project.dataset.table"]
+def obtain_table_strings(sql_string: str) -> List[str]:
+    
+    pattern = r'\b\w+\.(?!value\.)\w+\.\w+\b'
+    tables = re.findall(pattern, sql_string)
+    
+    return tables
 
 def render_table_string() -> str:
     return "{{ params['project_param'] }}.{{ params['dataset_param'] }}.{{ params['table_param'] }}"
