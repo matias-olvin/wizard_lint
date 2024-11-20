5136c507c8acb48e0de8f86d23aa02db0e185bb2
matias-olvin <matias@olvin.com>
2024-11-20 14:10:37 +0000

    add new tests for render.py to align with wanted behaviour

--- tests/test_render.py
+++ tests/test_render.py
@@ -12,5 +12,22 @@ config = {
 def test_render_table_string():
     assert render_table_string() == "{{ params['project_param'] }}.{{ params['dataset_param'] }}.{{ params['table_param'] }}"
 
-def test_obtain_table_strings():
-    assert obtain_table_strings() == ["project.dataset.table"]
+def test_obtain_table_strings_single():
+    sql_string = "SELECT * FROM project.dataset.table"
+    assert obtain_table_strings(sql_string) == ["project.dataset.table"]
+
+def test_obtain_table_strings_multiple():
+    sql_string = "SELECT * FROM project.dataset.table1 JOIN project.dataset.table2 ON table1.id = table2.id"
+    assert obtain_table_strings(sql_string) == ["project.dataset.table1", "project.dataset.table2"]
+
+def test_obtain_table_strings_no_match():
+    sql_string = "SELECT * FROM table"
+    assert obtain_table_strings(sql_string) == []
+
+def test_obtain_table_strings_mixed():
+    sql_string = "SELECT * FROM project.dataset.table1, dataset.table2 WHERE project.dataset.table3.id = table2.id"
+    assert obtain_table_strings(sql_string) == ["project.dataset.table1", "project.dataset.table3"]
+
+def test_obtain_table_strings_with_airflow_vars_and_no_tables():
+    sql_string = "SELECT * FROM table WHERE column = '{{ var.value.variable_name }}'"
+    assert obtain_table_strings(sql_string) == []
