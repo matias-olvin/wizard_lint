c6aa3cc8ce39c9b4f3f4623e5a8fd22f92266eba
matias-olvin <matias@olvin.com>
2024-11-21 09:17:08 +0000

    Refactor main.py to use config mapping for rendering table strings

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from wizard_lint.retrieve import obtain_file_paths_from_directory, obtain_sql_string_from_file_path
-from wizard_lint.render import obtain_table_strings
+from wizard_lint.retrieve import obtain_file_paths_from_directory, obtain_sql_string_from_file_path, obtain_config_yaml
+from wizard_lint.render import obtain_table_strings, render_table_string
 
 
 def main():
+
+    config = obtain_config_yaml("./config.yaml")
 
     # obtain file paths from input directory
     file_paths = obtain_file_paths_from_directory("./sql_folder")
@@ -11,7 +13,11 @@ def main():
     sql_strings = [obtain_sql_string_from_file_path(path) for path in file_paths]
 
     for sql_string in sql_strings:
-        print(obtain_table_strings(sql_string))
+        table_strings = obtain_table_strings(sql_string)
+
+        rendered_table_strings_mapping = {table_string: render_table_string(config, table_string) for table_string in table_strings}
+
+        print(rendered_table_strings_mapping)
 
 if __name__=="__main__":
     main()
