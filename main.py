ac512340bd76cc93e5e76452ec8e84be7db983e7
matias-olvin <matias@olvin.com>
2024-11-21 15:47:55 +0000

    Refactor main.py to overwrite SQL files with rendered SQL strings

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from wizard_lint.retrieve import obtain_file_paths_from_directory, obtain_sql_string_from_file_path, obtain_config_yaml
-from wizard_lint.render import obtain_table_strings, render_table_string, render_sql_string_with_mapping_dict
+from wizard_lint.render import obtain_table_strings, render_table_string, render_sql_string_with_mapping_dict, overwrite_sql_file_with_rendered_sql_string
 
 
 def main():
@@ -9,19 +9,17 @@ def main():
     # obtain file paths from input directory
     file_paths = obtain_file_paths_from_directory("./sql_folder")
 
-    # obtain sql from file paths obtained above
-    sql_strings = [obtain_sql_string_from_file_path(path) for path in file_paths]
+    for path in file_paths:
 
-    rendered_sql_strings = list()
+        sql_string = obtain_sql_string_from_file_path(path)
 
-    for sql_string in sql_strings:
         table_strings = obtain_table_strings(sql_string)
 
         rendered_table_strings_mapping = {table_string: render_table_string(config, table_string) for table_string in table_strings}
 
         rendered_sql_string = render_sql_string_with_mapping_dict(sql_string, rendered_table_strings_mapping)
 
-        rendered_sql_strings.append(rendered_sql_string)
+        overwrite_sql_file_with_rendered_sql_string(path, rendered_sql_string)
 
 if __name__=="__main__":
     main()
