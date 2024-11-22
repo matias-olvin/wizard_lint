6e342f78ebb222183b60487ef4bc46064a04f51a
matias-olvin <matias@olvin.com>
2024-11-22 08:43:10 +0000

    update tests for obtain table strings

--- tests/test_render.py
+++ tests/test_render.py
@@ -6,12 +6,12 @@ from wizard_lint.render import (obtain_table_strings,
 
 # test obtain_table_strings
 def test_obtain_table_strings_single():
-    sql_string = "SELECT * FROM project.dataset.table"
+    sql_string = "SELECT * FROM `project.dataset.table`"
     assert obtain_table_strings(sql_string) == ["project.dataset.table"]
 
 
 def test_obtain_table_strings_multiple():
-    sql_string = "SELECT * FROM project.dataset.table1 JOIN project.dataset.table2 ON table1.id = table2.id"
+    sql_string = "SELECT * FROM `project.dataset.table1` JOIN `project.dataset.table2` ON table1.id = table2.id"
     assert obtain_table_strings(sql_string) == [
         "project.dataset.table1",
         "project.dataset.table2",
@@ -19,21 +19,16 @@ def test_obtain_table_strings_multiple():
 
 
 def test_obtain_table_strings_no_match():
-    sql_string = "SELECT * FROM table"
+    sql_string = "SELECT * FROM `table`"
     assert obtain_table_strings(sql_string) == []
 
 
 def test_obtain_table_strings_mixed():
-    sql_string = "SELECT * FROM project.dataset.table1, dataset.table2 WHERE project.dataset.table3.id = table2.id"
+    sql_string = "SELECT * FROM `project.dataset.table1`, `dataset.table2` WHERE `project.dataset.table3`.id = table2.id"
     assert obtain_table_strings(sql_string) == [
         "project.dataset.table1",
         "project.dataset.table3",
     ]
-
-
-def test_obtain_table_strings_with_airflow_vars_and_no_tables():
-    sql_string = "SELECT * FROM table WHERE column = '{{ var.value.variable_name }}'"
-    assert obtain_table_strings(sql_string) == []
 
 
 # test render_table_string
