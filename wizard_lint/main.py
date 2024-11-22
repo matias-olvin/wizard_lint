9609bf0fcc988ef19422c74a011c4f3bb24b7880
matias-olvin <matias@olvin.com>
2024-11-22 09:30:35 +0000

    move main to wizard_lint package

--- wizard_lint/main.py
+++ wizard_lint/main.py
@@ -1,11 +1,11 @@
 import argparse
 from rich import print
 
-from wizard_lint.render import (obtain_table_strings,
+from render import (obtain_table_strings,
                                 overwrite_sql_file_with_rendered_sql_string,
                                 render_sql_string_with_mapping_dict,
                                 render_table_string)
-from wizard_lint.retrieve import (obtain_config_yaml,
+from retrieve import (obtain_config_yaml,
                                   obtain_file_paths_from_directory,
                                   obtain_sql_string_from_file_path)
 
